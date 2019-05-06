27350cc717400cfb4c0b55ca1ab677f3ce0577b1
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2019-05-06 13:25:52 +1200

    navigator(worker added)

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,7 @@
 var title = document.querySelector('.title');
 var courseFeatureElements = document.querySelectorAll('.course-feature');
 var button = document.querySelector('button');
-
+navigator.serviceWorker.register('/sw.js');
 
 function animate() {
   title.classList.remove('animate-in');

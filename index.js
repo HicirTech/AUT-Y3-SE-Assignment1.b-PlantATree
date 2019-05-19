8f4228d6fda12861d4bc0559c0915ad2dd694e50
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2019-05-19 19:06:42 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+console.log("is the the file")
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))

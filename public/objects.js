cb72d22b848beef02d754a5e52e8908016764da7
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2019-05-20 02:30:00 +1200

    split test

---
+++ public/objects.js
@@ -0,0 +1,21 @@
+
+let tipData = {
+    tips : {
+        topic:['Tree needs water','Tree need sun light','Tree takes space to grow'],
+        content:['you need to give tree enough water', 'you should not plant your tree on a undergrand garage','You should not plant tree inside your wardrobe']
+    }
+};
+
+let treeData = {
+  trees : {
+      name:['TreeA','TreeB','TreeC','TreeD'],
+      desc:['DescA', 'DescB','DescC','DescD'],
+      price:['1','2','3','4']
+  }
+};
+let newData = {
+  news : {
+      date:['19/5/2019'],
+      desc:['Team is working on this page']
+  }
+};

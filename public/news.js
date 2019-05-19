6e7c8003510dec50a8c996d7ee1b02120dba5b8a
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2019-05-20 02:35:40 +1200

    delete js

--- public/news.js
+++
@@ -1,9 +0,0 @@
-addNews();
-var news = '<div class="container"><p>%TIME%</p><li>%NEWS%</li><br></div>';  
-function addNews(){
-    for(var i = 0;i!=newData.news['date'].length;i++){
-      var html= news.replace('%TIME%',newData.news['date'][i]);
-      html= html.replace('%NEWS%',newData.news['desc'][i]);
-      newsC.insertAdjacentHTML("beforeend",html);
-    }
-  }

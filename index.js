262214360dbd8caf4daaf0e1b10654853964603f
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2019-05-19 22:14:52 +1200

    temp cutting edge

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-console.log("is the the file")
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  function getAWS (){
+    console.log("called a function");
+  }
+  getAWS();
--- test.js
+++ test.js
@@ -5,10 +5,10 @@ const test = require('tape');
 // Start the app
 const env = Object.assign({}, process.env, {PORT: 5000});
 const child = spawn('node', ['index.js'], {env});
-console.log("TEST >node JS ")
+
 test('responds to requests', (t) => {
   t.plan(4);
-
+  console.log("TEST >nodeJS ")
   // Wait until the server is ready
   child.stdout.on('data', _ => {
     // Make a request to our app
